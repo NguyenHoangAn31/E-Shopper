10fe590383292a495736367523be612867f73099
NguyenHoangAn31 <anphung311002@gmail.com>
2025-02-11 13:20:10 +0700

    add layout

---
+++ src/main/java/com/example/security/configs/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.security.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+      @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // String staticFolder = "file:///" + "D:/new_images/"; // Cập nhật đường dẫn đến thư mục ngoài ổ D
+        String staticFolder = "file:///" + System.getProperty("user.dir") + "/src/main/resources/static/";
+        registry.addResourceHandler("/**").addResourceLocations(staticFolder);
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
